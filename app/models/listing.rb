26eb10fd2c9c60ef90ddef2a2987ffed63fac80a
˜Ricardo <ricardodullius@gmail.com>
2015-01-08 16:44:29 -0200

    validation user list

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,6 @@ class Listing < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "50x>", :thumb => "100x100>" }, :default_url => "default.jpg"
 	validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
 
+validates :name, :description, :price, presence: true
+validates :price, numericality: { greater_than: 0 }
 end
