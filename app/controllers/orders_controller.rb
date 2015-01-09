12c134ed0e3b77cae6e64e8c5ef18a13fe92d62e
˜Ricardo <ricardodullius@gmail.com>
2015-01-09 17:44:05 -0200

    historico de pedidos

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,21 +2,22 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-  def index
-    @orders = Order.all
+  def sales
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
   end
 
-  def show
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
   end
 
+  # GET /orders/new
   def new
     @order = Order.new
     @listing = Listing.find(params[:listing_id])
   end
 
-  def edit
-  end
-
+  # POST /orders
+  # POST /orders.json
   def create
     @order = Order.new(order_params)
     @listing = Listing.find(params[:listing_id])
@@ -25,10 +26,10 @@ class OrdersController < ApplicationController
     @order.listing_id = @listing.id
     @order.buyer_id = current_user.id
     @order.seller_id = @seller.id
-  
-  respond_to do |format|
+
+    respond_to do |format|
       if @order.save
-        format.html { redirect_to root_url, notice: 'Pedido concluído com sucesso' }
+        format.html { redirect_to root_url, notice: 'Order was successfully created.' }
         format.json { render action: 'show', status: :created, location: @order }
       else
         format.html { render action: 'new' }
@@ -37,7 +38,7 @@ class OrdersController < ApplicationController
     end
   end
 
-    private
+  private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
       @order = Order.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,15 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :models
   resources :listings do
-  resources :orders
+  resources :orders, only: [:new, :create]
 end
 
   get 'pages/querover'
   get 'pages/sobre'
   get 'pages/contato'
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
 
   root 'listings#index'
 
